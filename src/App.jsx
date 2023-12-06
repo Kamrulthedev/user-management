a6e7811ba62655a28202f7b1b3479125c52b7217
Kamrul Hassan <kamrulthedev@gmail.com>
2023-12-06 14:09:49 +0600

    form-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [users, setUsers] = useState([]) // Provide an initial value of an empty array
 
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
 
   return (
     <>
-      
-      <h1>Uaer management system</h1>
-     
+      <h1>User management system</h1>
+    <h3>This is User on the:{users.length}</h3>
+
+    <form>
+    
+
+    </form>
+
+
+
+    {
+      users.map(user => <p key={user.id}> 
+      {user.id}.
+      {user.name} <br />
+      {user.email}
+      </p>)
+    }
     </>
   )
 }
